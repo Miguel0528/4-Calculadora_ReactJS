eaae5d3c75780516b343c27114c723b9493ca2cc
Miguel0528 <miguel.araujo0528@gmail.com>
2025-02-11 21:11:53 -0300

    Criação de componente

---
+++ src/components/Calculator.js
@@ -0,0 +1,19 @@
+
+
+function Calculator(){
+
+    const num = 1;
+
+
+    return(
+        <div>
+            <button className="w-[200px] h-[200px] bg-green-500">Adicionar</button>
+        </div>
+    )
+
+
+
+
+}
+
+export default Calculator;
